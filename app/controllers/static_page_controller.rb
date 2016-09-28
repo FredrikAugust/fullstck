cb025553bf0e7f1fcbbac79f267f9beee7f0db7a
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-09-28 22:16:35 +0200

    remove stupid ssl shit that doesn't even work

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,8 +1,4 @@
 class StaticPageController < ApplicationController
   def index
   end
-
-  def letsencrypt
-    render text: 'YpR6lWn-MytVCmF_cTlfmPe_XFFQSTUr_qRrwzAxUrk._8dzRWZ5-z5LI5mskBodjOPtbhrPaRnnQrpZuNbs58Q'
-  end
 end
