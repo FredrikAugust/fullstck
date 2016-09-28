802ed150d678e6f8061e43cf2482456d54dab57e
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-09-28 22:47:02 +0200

    moar ssl

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,8 @@
 class StaticPageController < ApplicationController
   def index
   end
+
+  def ssl
+    render text: 'NS7r8DchNM1-klaO1on2HRAVQAkqil7nizwfdG3u9vw.2-E9DzzYma7wPTE0h0Brc5A8hj4wXic-n7sgJg6Tv_w'
+  end
 end
