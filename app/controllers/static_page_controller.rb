8ef94621b81667cdc9f32cce79b3676ab3aef67b
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-09-28 22:00:28 +0200

    ssl auth

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,8 @@
 class StaticPageController < ApplicationController
   def index
   end
+
+  def letsencrypt
+    render text: 'o5RFge336MEvRasA8NWrSNhTuZFj1Z5KMggG-FSqF5U._8dzRWZ5-z5LI5mskBodjOPtbhrPaRnnQrpZuNbs58Q'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root '/', controller: :static_page, action: :index
+
+  get '/.well-known/acme-challenge/:id' => 'static_page#letsencrypt'
 end
