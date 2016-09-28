94979057ff5d35aea524c6136bf5802f340add58
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-09-28 23:01:50 +0200

    FIXED SSL <3<3

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,8 +1,4 @@
 class StaticPageController < ApplicationController
   def index
   end
-
-  def ssl
-    render text: 'LnAEt2Kp_IH6ZRchnLVKrRDC1G2XI4LnMFMCFCsmMz8.2-E9DzzYma7wPTE0h0Brc5A8hj4wXic-n7sgJg6Tv_w'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   root '/', controller: :static_page, action: :index
-  get '/.well-known/acme-challenge/:id' => 'static_page#ssl'
 end
