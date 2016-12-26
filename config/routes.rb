ccfed22636244851b6d3e02d32181cbebaf53301
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-12-26 22:05:22 +0100

    new ssl cert in the making

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root '/', controller: :static_page, action: :index
 
   post '/contact', controller: :static_page, action: :contact
+
+  get '/.well-known/acme-challenge/:code', controller: :static_page, action: :acme
 end
